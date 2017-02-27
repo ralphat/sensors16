f9ec4581c2f99996f3f04c7134104caf765645b9
rt <aarthi1995@gmail.com>
2017-02-27 23:34:20 +0530

    db credentials

--- connect.php
+++ connect.php
@@ -3,11 +3,11 @@
 //ini_set('display_errors', 1);
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "groves";
 $dbname = "sensors18";
 
 // Create connection
-$con = $db = new mysqli($servername, $username, $password, $dbname);
+$db = new mysqli($servername, $username, $password, $dbname);
 if ($db->connect_errno)
   {
   echo "Failed to connect to MySQL: " . $db->connect_error;
