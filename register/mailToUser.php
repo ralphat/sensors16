cdbd0e8c165b5c0eb84863783ab9ca48758601d1
rt <aarthi1995@gmail.com>
2017-02-27 21:26:45 +0530

    mail verification added

--- register/mailToUser.php
+++ register/mailToUser.php
@@ -1,33 +1,40 @@
 <?php
 require '../../PHPMailer/PHPMailerAutoload.php';
+$mail = new PHPMailer();  // create a new object
+    $mail->IsSMTP(); // enable SMTP
+    $mail->SMTPDebug = 2;  // debugging: 1 = errors and messages, 2 = messages only
+    $mail->SMTPAuth = true;  // authentication enabled
+    $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for GMail
+    $mail->SMTPAutoTLS = false;
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 587;
 
-//Create a new PHPMailer instance
-$mail = new PHPMailer;
-//Set who the mail->Body is to be sent from
-$mail->setFrom('noreply@sensors.org.in', 'Sensors 16');
-//Set who the mail->Body is to be sent to
-$mail->addAddress($_POST['email']);
-//Set the subject line
-$mail->Subject = 'Verification of Email account.';
+    $mail->Username = "admin@sensors.org.in";  
+    $mail->Password = "Sensors!1";           
+    $mail->SetFrom("admin@sensors.org.in", "Sensors");
+	$recipient_email = $_POST['email'];
+	$recipient_name = $_POST['name'];
+	$mail->AddAddress($recipient_email, $recipient_name);
+	$mail->Subject = 'Verification of Email account.';
 
-$mail->Body = '<html><body style="color:#000;line-height:normal">';
+	$mail->Body = '<html><body style="color:#000;line-height:normal">';
 
-$mail->Body .= '<p>Hello '.$_POST["name"].',</p><p>Greeting from Sensors\'16.</p><br /><p>This email was recently registered in sensors.org.in. Click the following link to activate your account and to generate your SENSORS ID.</p>';
+	$mail->Body .= '<p>Hello '.$_POST["name"].',</p><p>Greetings from Sensors\'17!</p><br /><p>Thank you for registering with us at sensors.org.in. Here are the credentials you provided during registration :<br />Username : '.$_POST["email"].'<br />Password : '.$_POST['password'].' <br />Hit the following link to activate your account and complete registration.</p>';
 
-$mail->Body .= '<p>sensors.org.in/16/register/verify.php?id='.md5($_POST["email"]).'</p><br /><p>Kindly ignore if you did no such registration.</p><p>Regards,<br />Sensors\'16 Team</p>';
+	$mail->Body .= '<p>sensors.org.in/register/verify.php?id='.md5($_POST["email"]).'</p><br /><p>Kindly ignore if you did no such registration.</p><p>Regards,<br />Sensors\'16 Team</p>';
 
- 
-$mail->Body .= '</body></html>';
+	 
+	$mail->Body .= '</body></html>';
 
-$mail->IsHTML(true);
+	$mail->IsHTML(true);
 
-$mail->AltBody  =  'http://sensors.org.in/16/register/verify.php?id='.md5($_POST["email"]);
-
-//send the mail->Body, check for errors
-if (!$mail->send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-    $output = "Registration not complete. Check the email ID provided.";
-} else {
-    $output = "Verification email sent. Click the link sent to your email ID to activate your account.";
-}
+	$mail->AltBody  =  'http://sensors.org.in/register/verify.php?id='.md5($_POST["email"]);
+	    
+    if(!$mail->Send()) {
+        $error = 'Mail error: '.$mail->ErrorInfo; 
+        return false;
+    } else {
+        $error = 'Message sent!';
+        return true;
+    }
 ?>
--- register/verify.php
+++ register/verify.php
@@ -1,74 +1,29 @@
 <?php
-
 ini_set('display_errors','off');
-
-if(empty($_GET["id"])){
-        header("Location: index.html");
-        exit;
-	}
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
 include '../connect.php';
-$hashedId = $_GET["id"];
 
-$mainCheck = "select * from `main` where MD5(Email) = '".$hashedId."'";
-$mainCheckQuery = mysql_query($mainCheck);
+$username = $_POST['id'];
 
-if(mysql_num_rows($mainCheckQuery)){
-$output = "User already registered.";
-$registration = "done";
+if($username){
+	$sql = "SELECT * FROM `main` where `email`='".$username."'";
+	if(!$result = $db->query($sql)){
+	    die('There was an error running the query [' . $db->error . ']');
+	}
+	if(mysqli_num_rows($result)){
+		$row = $result->fetch_assoc();
+		$id = $row['id'];
+		echo $id;
+		$update_query = "UPDATE `main` SET registered = 1 where `email`='".$username."'";
+		if(!$result = $db->query($update_query)){
+	    	die('There was an error running the query [' . $db->error . ']');
+		}
+	} else {
+		echo "Empty returned";
+	}
+} else {
+	echo "Empty string";
 }
-else{
-$checkIfExist = "insert into main(Name,DOB,Email,Password,Mobile,College,Department,Year,Gender) select * from `temp` where MD5(Email) = '".$hashedId."'";
-
-if(mysql_query($checkIfExist))
-$output = "User registered.";
-else{
-	header("Location: index.html");
-        exit;
-}
-}
-$id = mysql_fetch_array(mysql_query($mainCheck));
-$output = $output." Your Sensors'16 ID is ".$id['ID'];
-if($registration!="done"){
-$create_registration_query = "insert into registration(ID) values (".$id['ID'].")";
-mysql_query($create_registration_query);
-}
+$db->close();
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Sensors'16</title>
-	<meta name="author" content="Sarwesh Krishnan" />
-	<meta name="description" content="Instrumentation and Contol Engineering department, NITT symposium website." />
-	<meta name="keywords"  content="sensors,symposium,NITT,ICE,instrumentation,control" />
-	<meta name="Resource-type" content="Document" />
-	
-	<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
-	
-	<style>
-	body{
-	background-color:#024986;
-	}
-	
-	h1{
-	font-family:'Source Sans Pro';
-	margin:250px auto;
-	color:#fff;
-	}
-	
-	a{
-	font-color:#000;
-	text-decoration:none;
-	color:#000;
-	}
-	a:hover{
-	border-bottom:1px solid #000;
-	}
-	</style>
-</head>
-<body>
-<center><h1><?php echo $output;?></h1></center>
-<br />
-<center><a href="./index.html">Create another account</a></center>
-</body>
-</html>
