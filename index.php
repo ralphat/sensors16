cc2a628dd211c7cca5755c1c2748a24938d2250c
rt <aarthi1995@gmail.com>
2017-03-02 15:42:13 +0530

    added uniq - sponsor

--- index.php
+++ index.php
@@ -2,13 +2,12 @@
 <html>
 
 <head>
-        <meta name="google-site-verification" content="AKxlCqUFO0nn7TVv3ocOMbn_oGaKuBOLnWkEI04upoE" />
+    <meta name="google-site-verification" content="AKxlCqUFO0nn7TVv3ocOMbn_oGaKuBOLnWkEI04upoE" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	
 	<meta name="viewport" content="height=device-height, initial-scale=0.5, maximum-scale=1"/>
 
 	<title>Sensors'17</title>
-	<meta name="author" content="Sarwesh Krishnan" />
 	<meta name="description" content="Instrumentation and Contol Engineering department, NITT symposium website." />
 	<meta name="keywords"  content="sensors,symposium,NITT,ICE,instrumentation,control" />
 	<meta name="Resource-type" content="Document" />
@@ -30,18 +29,9 @@
   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=1063823873635962";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-<!--
-<div id="form-container">
-<?php
-if(isset($_COOKIE['SID']))
-echo 'Logged in as SID '.$_COOKIE['SID'].'. <button id="logout"> Logout</button>';
-else
-echo '<button id="login">Login</button> / <a target="_blank" href="./register"> Register</a>';
-?>
-</div>
--->
+
 <div class="parallax">
-	<!--<a target="_blank" href="http://www.uniqtechnologies.co.in/"><img id="uniq" src="./uniq/uniq.jpg" /></a>-->
+	<a target="_blank" href="http://www.uniqtechnologies.co.in/"><img id="uniq" src="./uniq/uniq.jpg" /></a>
 	<div><img id="icea_logo" src="./website/icea_logo.png" /></div>
 	<div><img id="sensors_middle" src="./website/sensors_middle.png" /></div>
 
@@ -98,7 +88,7 @@ Tip: Try to remove the background-attachment property to remove the scrolling ef
 </form>
 </div>
 
-<!--
+
 <div id="uniq-container">
 <div id="uniq-link">
 <hr>
@@ -109,9 +99,9 @@ Tip: Try to remove the background-attachment property to remove the scrolling ef
 <hr>
 <div class="fb-like" data-href="https://www.facebook.com/uniqtechnologies/" data-layout="button" data-action="like" data-show-faces="true" data-share="false"></div>
 </div>
-<div>Sponsor Name Here</div>
+<div>UNIQ Technologies</div>
 </div>
--->
+
 <div class="fb-like" id="fbLike" style="position:absolute;top:94.5%;right:4%;" data-href="https://www.facebook.com/sensorsNITTrichy/" data-layout="button" data-action="like" data-show-faces="true" data-share="true"></div>
 
 
