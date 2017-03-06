a3b82fdad9c0085d903d1d2325734db8c773e5c1
rt <aarthi1995@gmail.com>
2017-03-06 18:34:52 +0530

    responsive

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
     <meta name="google-site-verification" content="AKxlCqUFO0nn7TVv3ocOMbn_oGaKuBOLnWkEI04upoE" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	
-	<meta name="viewport" content="height=device-height, initial-scale=0.5, maximum-scale=1"/>
-
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Sensors'17</title>
 	<meta name="description" content="Instrumentation and Contol Engineering department, NITT symposium website." />
 	<meta name="keywords"  content="sensors,symposium,NITT,ICE,instrumentation,control" />
@@ -14,6 +13,7 @@
 	
     <link rel = "shortcut icon" href = "./tab_image.ico"/>
 	<link type="text/css" rel="stylesheet" href="normalise.css" />
+	<link type="text/css" rel="stylesheet" href="bootstrap.css" />
 	<link type="text/css" rel="stylesheet" href="style_edited.css" />
 	<link type="text/css" rel="stylesheet" href="common.css" />
         
@@ -30,18 +30,39 @@
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
-<div class="parallax">
-	<a target="_blank" href="http://www.uniqtechnologies.co.in/"><img id="uniq" src="./uniq/uniq.jpg" /></a>
-	<div><img id="icea_logo" src="./website/icea_logo.png" /></div>
-	<div><img id="sensors_middle" src="./website/sensors_middle.png" /></div>
-
-	<a href="./workshops"><img id="workshops-link" src="./website/workshops.png"></a>	
-	<a href="./events"><img id="events-link" src="./website/events.png"></a>
-	<a href="./about"><img id="about-link" src="./website/about.png"></a>
-	<a href="./contact"><img id="contact-link" src="./website/contact.png"></a>
+<div class="parallax container-fluid">
+	<div class="row" style="display: flex; align-items: center;">
+		<div class="col-md-2 col-sm-2 col-xs-3">
+			<a target="_blank" href="http://www.uniqtechnologies.co.in/"><img class="img-responsive img-thumbnail" src="./uniq/uniq.jpg" /></a>
+		</div>
+		<div class="col-md-2 col-md-offset-8 col-sm-2 col-sm-offset-8 col-xs-3 col-xs-offset-6">
+			<div><img class="img-responsive" src="./website/icea_logo1.png" /></div>		
+		</div>
+	</div>
+	<div class="row block1" style="z-index: 1;">
+		<div class="col-lg-3 col-lg-offset-3 col-md-3 col-md-offset-3 col-sm-4 col-sm-offset-2 col-xs-5 col-xs-offset-1 workshops-link" >
+			<a href="./workshops"><img class="img-responsive" src="./website/workshops.png"></a>	
+		</div>
+		<div class="col-lg-3 col-lg-offset-0 col-md-3 col-md-offset-0 col-sm-4 col-sm-offset-0 col-xs-6 events-link" >
+			<a href="./events"><img class="img-responsive" src="./website/events.png"></a>
+		</div>
+	</div>
+	<div class="row block2">
+		<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 sensors_middle">
+			<img class="img-responsive" src="./website/sensors_middle.png">
+		</div>
+	</div>
+	<div class="row block3" style="z-index: 1;">
+		<div class="col-lg-3 col-lg-offset-3 col-md-3 col-md-offset-3 col-sm-4 col-sm-offset-2 col-xs-5 col-xs-offset-1 about-link" >
+			<a href="./about"><img class="img-responsive" src="./website/about.png"></a>	
+		</div>
+		<div class="col-lg-3 col-lg-offset-0 col-md-3 col-md-offset-0 col-sm-4 col-sm-offset-0 col-xs-6 col-xs-offset-0 contact-link" >
+			<a href="./contact"><img class="img-responsive" src="./website/contact.png"></a>
+		</div>
+	</div>
 
 <div id="sca">
-	<h1>Sense&nbsp;.&nbsp;Control&nbsp;.&nbsp;Actuate&nbsp;.</h1>
+	<p>Sense&nbsp;.&nbsp;Control&nbsp;.&nbsp;Actuate&nbsp;.</p>
 </div>
 
 <div id="form-container">
